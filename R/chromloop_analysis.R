031612180fdc51e4d0498d2158feefab85f0596b
ibn-salem <jonas.ibn-salem@gmx.de>
2017-03-21 18:13:38 +0100

    update analysis script

--- R/chromloop_analysis.R
+++ R/chromloop_analysis.R
@@ -5,46 +5,57 @@
 
 require(chromloop)   # devtools::install_github("ibn-salem/chromloop")
 require(tidyverse)
+require(stringr)
 
 
-# parse metatdata table for input files
+# 0) Set parameter --------------------------------------------------------
 
-meta <- read_tsv("data/ENCODE/metadata.flt.tsv")
 
-# DEBUG: 
 # work only on subset here:
-meta <- meta %>% top_n(3)
-#-------------------------------------------------------------------------------
-# 1) take motifs
-#-------------------------------------------------------------------------------
+
+useTFs <- c(
+  "ZNF143",
+  "STAT1",
+  "ZNF274",
+  "ZNF384",
+  "CTCF",
+  "RAD21",
+  "POLR2A",
+  "NFYB"
+)
+
+
+# 1) select motifs and parse input data -----------------------------------
 ancGR <- chromloop::motif.hg19.CTCF
 
-#-------------------------------------------------------------------------------
-# 2) annotae with coverage 
-#-------------------------------------------------------------------------------
-covRLle <- lapply(meta$filePath, parseBigWigToRle, seqInfo = seqinfo(ancGR))
+# parse metatdata table for input files
+all_metadata <- read_tsv("data/ENCODE/metadata.flt.tsv")
 
-#-------------------------------------------------------------------------------
-# 3) build InteractonSet object
-#-------------------------------------------------------------------------------
+# filter input data
+meta <- all_metadata %>% 
+  filter(TF %in% str_to_upper(useTFs)) %>%
+  mutate(TF = parse_factor(TF, levels = useTFs)) %>%
+  arrange(TF)
 
+# DEBUG: use only the first two ChIP-seq data sets
+meta <- head(meta, 2)
 
-#-------------------------------------------------------------------------------
-# 4) compute correlations
-#-------------------------------------------------------------------------------
+# 2) annotae with coverage ------------------------------------------------
+covRle <- lapply(meta$filePath, parseBigWigToRle, seqInfo = seqinfo(ancGR))
 
+for (i in seq_along(meta$TF)) {
+  ancGR <- chromloop::addCovToGR(ancGR, covRle[[i]], colname = paste0("cov_", meta$TF[i]))
+}
 
-#-------------------------------------------------------------------------------
-# 5) add ture loops
-#-------------------------------------------------------------------------------
+# 3) build InteractonSet object -------------------------------------------
+gi <- chromloop::getCisPairs(ancGR, maxDist = 10^6)
 
+# 4) compute correlations -------------------------------------------------
+gi <- chromloop::applyToCloseGI(gi, cor, str_c("cor_", meta$TF[1]))
 
-#-------------------------------------------------------------------------------
-# 6) predict loops
-#-------------------------------------------------------------------------------
+# 5) add ture loops -------------------------------------------------------
 
+# 6) predict loops --------------------------------------------------------
 
-#-------------------------------------------------------------------------------
-# 7)  Analyse performace
-#-------------------------------------------------------------------------------
+# 7)  Analyse performace --------------------------------------------------
 
