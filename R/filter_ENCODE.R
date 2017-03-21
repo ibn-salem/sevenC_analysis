ddca23f3a1cae88025bff0150cbe379934c45f8e
ibn-salem <jonas.ibn-salem@gmx.de>
2017-03-21 18:13:20 +0100

    update draft filter_ENCODE.R script to selct proper output / replicate combination

--- R/filter_ENCODE.R
+++ R/filter_ENCODE.R
@@ -5,6 +5,17 @@
 require(tidyverse)
 require(stringr)
 
+useTFs <- c(
+  "ZNF143",
+  "STAT1",
+  "ZNF274",
+  "ZNF384",
+  "CTCF",
+  "RAD21",
+  "POLR2A",
+  "NFYB"
+)
+
 #-------------------------------------------------------------------------------
 # parse meta data and filter
 #-------------------------------------------------------------------------------
@@ -16,16 +27,53 @@ meta <- read_tsv(metaDataFile)
 
 # add TF name
 df <- meta %>%
-  mutate(TF = str_replace(`Experiment target`, "-human", ""))
+  mutate(TF = str_replace(`Experiment target`, "-human", "")) %>%
+  select(`File accession`, TF, `Output type`, 
+         `Biological replicate(s)`, Lab, everything())
+
+# utils:::format.object_size(x, format="auto")
+
+plotDF <- df %>%
+  filter(`Biosample term name` == "GM12878") %>%
+  filter(Lab == "ENCODE Processing Pipeline") %>%
+  mutate(size = parse_double(Size)) %>%
+  group_by(`Output type`, `Biological replicate(s)`) %>%
+  summarise(
+      mean_size = mean(size, na.rm=TRUE),
+      n = n()
+  )
+
+# utils:::format.object_size(plotDF$mean_size, "Mb")
+
+plotDF %>%
+  ggplot(aes(x=`Output type`, y=n, fill=`Biological replicate(s)`)) + 
+  geom_bar(stat="identity", position="dodge") +
+  theme_bw() + 
+  theme(axis.text.x=element_text(angle=45, hjust=1))
 
 
+plotDF %>%
+  ggplot(aes(x=`Output type`, y=mean_size, fill=`Biological replicate(s)`)) + 
+  geom_bar(stat="identity", position="dodge") +
+  theme_bw() + 
+  theme(axis.text.x=element_text(angle=45, hjust=1))
+
+  
 # filter for "signal" as output with combined replicate (is NA here) and only GM12878 cell line
 
 flt <- df %>% 
-  filter(`Output type` == "signal") %>%
-  filter(is.na(`Biological replicate(s)`)) %>% 
+  filter(`Output type` %in% c("raw signal", "fold change over control", "signal")) %>%
+  filter(`Output type` %in% c("raw signal", "fold change over control")) %>%
+  filter(`Biological replicate(s)` == "1" | is.na(`Biological replicate(s)`)) %>% 
   filter(`Biosample term name` == "GM12878") %>%
-  distinct(TF, .keep_all = TRUE)
+  filter(TF %in% useTFs)
+  
+
+# is.na(`Biological replicate(s)`) | 
+# %>%
+#   distinct(TF, Lab, .keep_all = TRUE)
+
+
 
 # add location
 flt$filePath <- file.path("data", "ENCODE", "Experiments", basename(flt$`File download URL`))
