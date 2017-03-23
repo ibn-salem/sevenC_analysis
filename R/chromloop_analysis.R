1d69d7bb2b23468c8c62945c783d3d8e76bf4e22
ibn-salem <jonas.ibn-salem@gmx.de>
2017-03-23 18:40:44 +0100

    implement analysis pipline for parsing ChIP-seq coverage and annotation of correlation for loop anchor pairs

--- R/chromloop_analysis.R
+++ R/chromloop_analysis.R
@@ -29,29 +29,44 @@ useTFs <- c(
 ancGR <- chromloop::motif.hg19.CTCF
 
 # parse metatdata table for input files
-all_metadata <- read_tsv("data/ENCODE/metadata.flt.tsv")
+meta <- read_tsv("data/ENCODE/metadata.flt.tsv")
+
+# # filter input data
+# meta <- meta %>% 
+#   filter(TF %in% useTFs) %>%
+#   mutate(TF = parse_factor(TF, levels = useTFs)) %>%
+#   arrange(TF)
 
 # filter input data
-meta <- all_metadata %>% 
-  filter(TF %in% str_to_upper(useTFs)) %>%
-  mutate(TF = parse_factor(TF, levels = useTFs)) %>%
+meta <- meta %>% 
+  filter(TF %in% useTFs) %>%
+  filter(`Output type` == "fold change over control") %>%
+  mutate(TF = parse_factor(TF, intersect(useTFs, .$TF))) %>%
   arrange(TF)
 
-# DEBUG: use only the first two ChIP-seq data sets
-meta <- head(meta, 2)
+# # DEBUG use only two examples here:
+# meta <- meta %>%
+#   filter(TF %in% c("POLR2A", "CTCF"))
 
 # 2) annotae with coverage ------------------------------------------------
-covRle <- lapply(meta$filePath, parseBigWigToRle, seqInfo = seqinfo(ancGR))
+
+# covRle <- lapply(meta$filePath, parseBigWigToRle, seqInfo = seqinfo(ancGR))
 
 for (i in seq_along(meta$TF)) {
-  ancGR <- chromloop::addCovToGR(ancGR, covRle[[i]], colname = paste0("cov_", meta$TF[i]))
+  ancGR <- chromloop::addCovToGR(ancGR, meta$filePath[i], window = 10, 
+                                 colname = paste0("cov_", meta$TF[i]))
 }
 
 # 3) build InteractonSet object -------------------------------------------
 gi <- chromloop::getCisPairs(ancGR, maxDist = 10^6)
 
 # 4) compute correlations -------------------------------------------------
-gi <- chromloop::applyToCloseGI(gi, cor, str_c("cor_", meta$TF[1]))
+for (i in seq_along(meta$TF)) {
+  gi <- chromloop::applyToCloseGI(gi, 
+                                  datcol = paste0("cov_", meta$TF[i]),
+                                  fun = cor, 
+                                  colname = paste0("cor_", meta$TF[i]))
+}
 
 # 5) add ture loops -------------------------------------------------------
 
