a7cf14c7d15fe916ed8738964179d7d9347ec8ec
ibn-salem <jonas.ibn-salem@gmx.de>
2017-08-17 19:20:28 +0200

    add some helper functions for tidy modeling

---
+++ R/chromloop.functions.R
@@ -0,0 +1,77 @@
+#' fitts a logit model to training data and returns a tidy model fit.
+#' 
+#' @see broom::tidy()
+#' 
+#' @param splits an individual \code{rsplit} object.
+#' @param design a formular object defining the prediction design and variables.
+tidy_fitter <- function(splits, formula, ...) {
+  
+  # fit glm model
+  mod <- glm(formula = formula, 
+             data = analysis(splits), 
+             family = binomial(),
+             model = FALSE,  
+             x = FALSE,
+             ...)
+  
+  return(broom::tidy(mod))
+}
+
+
+#' Predict outcome using logistic regresstion.
+#' 
+#' Source: 
+#' https://stackoverflow.com/questions/25695565/predict-with-arbitrary-coefficients-in-r
+#' 
+#' @param data  A data.frame with predictor variables
+#' @param formula A modelling formula
+#' @param betas A vector with parameter estimates for predictor variables
+#'   
+#' @return a data.frame like \cod{df} with additional columns with the 
+#'   predictions.
+#'   
+#' @importFrom boot inv.logit
+#' @importFrom tibble as_tibble
+#' @importFrom dplyr mutate select
+pred_logit <- function(data, formula, betas){
+  
+  # save option and set na.action to pass
+  op <- options(na.action = 'na.pass')
+  on.exit(options(op))
+  
+  # build model matrix
+  mat <- modelr::model_matrix(data, formula)
+  
+  pred <- as.numeric(betas %*% t(mat)) %>%
+    boot::inv.logit()
+  
+  return(pred)
+  
+}
+
+# data <- assessment(dfCV$splits[[1]])
+# formula <- dfCV$design[[1]]
+# betas <- dfCV$tidy_model[[1]]$estimate
+# pred <- pred_logit(data, formula, betas)
+
+test_pred_logit <- function(){
+  
+  # testing
+  formula <- am ~ wt + cyl + hp 
+  mod <- glm(formula, mtcars, family = binomial())
+  betas <- coef(mod)
+  
+  pred <- pred_logit(mtcars, betas, formula = formula)
+  pred_glm <- predict.glm(mod, newdata = mtcars, type = "response")
+  names(pred_glm) <- NULL
+  
+  # tibble(
+  #   pred_glm = pred_glm, 
+  #   pred = pred
+  # )
+  stopifnot(all.equal(pred, pred_glm))
+}
+
+test_pred_logit()
+
+
