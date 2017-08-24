19682c488c22d87da4e596f37f418e1bb895ea8d
ibn-salem <jonas.ibn-salem@gmx.de>
2017-08-24 20:24:33 +0200

    add loop_feature_EDA.R script for exploratory data analysis on subset of data

---
+++ R/loop_features_EDA.R
@@ -0,0 +1,131 @@
+################################################################################
+# Analysis of predictd chromatin looping interactions using the chromloop tool
+################################################################################
+
+
+# require(chromloop)    # devtools::install_github("ibn-salem/chromloop")
+require(tidyverse)    # for tidy data
+require(stringr)      # for string functions
+require(RColorBrewer)   # for nice colors
+require(feather)      # for efficient storing of data.frames
+
+source("R/chromloop.functions.R")
+
+
+# 0) Set parameter --------------------------------------------------------
+
+# use previously saved gi object?
+
+MIN_MOTIF_SIG <- 6
+WINDOW_SIZE <- 1000
+BIN_SIZE <- 1
+
+COL_TF <- grDevices::colors()[str_detect(grDevices::colors(), "^((?!(gr(a|e)y|white)).)*[^\\d]$")]
+COL_TF <- COL_TF[!COL_TF %in% c("white", "black", "aliceblue", "azure", "beige", "bisque", "cornsilk", "cyan", "darkorchid", "coral", "darkmagenta")]
+# pie(rep(1, length(COL_TF)), col = COL_TF, labels = COL_TF, main = length(COL_TF))
+# COL_TF = colorRamps::primary.colors(124)
+
+COL_LOOP = brewer.pal(8, "Dark2")[c(8,5)] #[c(2,1,5,6)]
+names(COL_LOOP) <- c("No loop", "Loop")
+
+outPrefix <- file.path("results", paste0("v03_screen_TF_qfraq.", 
+                                         paste0("motifSig", MIN_MOTIF_SIG), 
+                                         "_w", WINDOW_SIZE, 
+                                         "_b", BIN_SIZE))
+
+dir.create(dirname(outPrefix), showWarnings = FALSE)
+
+
+# metadata file
+metaFile <- "data/ENCODE/metadata.fltBam.tsv"
+
+SELECTED_TF <- c(
+  "RAD21",
+  "SMC3",
+  "CTCF",
+  "ZNF143",
+  "STAT1",
+  "STAT3",
+  "NFYB"
+)
+
+#-------------------------------------------------------------------------------
+# Parse and filter input ChiP-seq data  -----------------------------------
+#-------------------------------------------------------------------------------
+
+# parse ucscMeta file
+meta <- read_tsv(metaFile,
+                 col_types = 
+                   cols(
+                     `File accession` = col_character(),
+                     TF = col_character(),
+                     `Output type` = col_character(),
+                     file_nrep = col_character(),
+                     exp_nrep = col_integer(),
+                     Lab = col_character(),
+                     filePath = col_character()
+                   )
+)
+
+# reformat metadata
+meta <- meta %>% 
+  mutate(name = TF) %>%
+  mutate(filePath = str_c(filePath, "-qfrags_allChr_chip.bed.sorted.bedGraph.bw")) %>% 
+  # filter(file.exists(filePath)) %>% 
+  select(TF, name, filePath, everything())
+
+# meta <- meta[1:3, ]
+
+#-------------------------------------------------------------------------------
+# load main data set with loops and features
+#-------------------------------------------------------------------------------
+# write_feather(df, paste0(outPrefix, ".df.feather"))
+# df <- read_feather(paste0(outPrefix, ".df.feather"))
+# write_feather(edaDF, paste0(outPrefix, ".edaDF.feather"))
+
+edaDF <- read_feather(paste0(outPrefix, ".df.feather"))
+
+# make a tidy DF
+tidyDF <- edaDF %>% 
+  gather(starts_with("cor_"), key = name, value = cor) %>% 
+  mutate(name = str_sub(name, 5))
+
+tidySeltDF <- tidyDF %>% 
+  filter(name %in% SELECTED_TF)
+
+# ------------------------------------------------------------------------------
+# Compare Correlation with Boxplot
+# ------------------------------------------------------------------------------
+
+tidySubDF <- tidyDF %>% 
+  sample_n(min(nrow(tidyDF), 10^7))
+
+p <- ggplot(tidySubDF, aes(x = name, y = cor, col = loop)) +
+  geom_boxplot() +
+  scale_color_manual(values = COL_LOOP, guide_legend(title = "")) +
+  theme_bw() + 
+  theme(
+    # text = element_text(size=20), 
+    # axis.text.x=element_blank(), 
+    legend.position = "bottom",
+    axis.text.x = element_text(angle = 60, hjust = 1)) + 
+  labs(y = "Correlation of ChIP-seq signal", x="")
+ggsave(p, file = paste0(outPrefix, ".EDA.cor.by_TF_and_loop.boxplot.pdf"), w = 28, h = 7)
+
+
+p <- ggplot(tidySeltDF, aes(x = name, y = cor, col = loop)) +
+  geom_boxplot() +
+  scale_color_manual(values = COL_LOOP, guide_legend(title = "")) +
+  theme_bw() + 
+  theme(
+    # text = element_text(size=20), 
+    # axis.text.x=element_blank(), 
+    legend.position = "bottom",
+    axis.text.x = element_text(angle = 60, hjust = 1)) + 
+  labs(y = "Correlation of ChIP-seq signal", x="")
+ggsave(p, file = paste0(outPrefix, ".EDA.selectTF.cor.by_TF_and_loop.boxplot.pdf"), w = 14, h = 7)
+
+
+
+#===============================================================================
+#===============================================================================
