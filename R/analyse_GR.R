d9bb077bd62528ef70a759f91d8cff66f6363198
ibn-salem <jonas.ibn-salem@gmx.de>
2017-08-31 15:45:29 +0200

    update peak to tss assignment analysis

--- R/analyse_GR.R
+++ R/analyse_GR.R
@@ -4,17 +4,19 @@
 
 
 require(chromloop)    # devtools::install_github("ibn-salem/chromloop")
+require(rtracklayer)  # to import() BED files
+require(EnsDb.Hsapiens.v75)
 require(TxDb.Hsapiens.UCSC.hg19.knownGene)  # for human genes
 require(tidyverse)    # for tidy data
 require(stringr)      # for string functions
 require(modelr)       # for tidy modeling
 require(precrec)      # for ROC and PRC curves
 require(RColorBrewer)   # for nice colors
-require(rtracklayer)  # to import() BED files
 require(rsample)
 require(pryr) # for object_size()
 require(feather)      # for efficient storing of data.frames
 require(multidplyr)   # for partition() and collect() to work in parallel
+require(networkBMA)
 source("R/chromloop.functions.R")
 
 
@@ -29,8 +31,9 @@ outPrefix <- "results/v03_screen_TF_qfraq.motifSig6_w1000_b1.GR"
 
 # metadata file
 metaFile <- "data/GR_CHIP-SEQ_FILES.tsv"
-peak_files <- c("data/Vockley2016/ChIP-seq/GSM2095218_DEX_3hr_GR_ChIPseq.Rep1_peaks.bed",
-                "data/Vockley2016/ChIP-seq/GSM2095219_DEX_3hr_GR_ChIPseq.Rep2_peaks.bed")
+# peak_files <- c("data/Vockley2016/ChIP-seq/GSM2095218_DEX_3hr_GR_ChIPseq.Rep1_peaks.bed",
+#                 "data/Vockley2016/ChIP-seq/GSM2095219_DEX_3hr_GR_ChIPseq.Rep2_peaks.bed")
+peak_file = "data/GR_BAM/ENCFF080RMP.bed"
 
 GR_fc_file = "data/GR_BAM/ENCFF678TFX.bigWig"
 # GR_fc_file = "data/ENCODE/Experiments/ENCFF563WQV.bam-qfrags_allChr_chip.bed.sorted.bedGraph.bw"
@@ -62,7 +65,10 @@ mcols(gi) <- as.data.frame(mcols(gi)) %>%
 
 
 # parse peaks
-peaks <- map(peak_files, rtracklayer::import.bed, seqinfo = seqinfo(gi))
+extraCols_narrowPeak <- c(signalValue = "numeric", pValue = "numeric",
+                          qValue = "numeric", peak = "integer")
+peaksGR <- rtracklayer::import(peak_file, format = "BED",
+                        extraCols = extraCols_narrowPeak, seqinfo = seqinfo(gi))
 
 # differntial expression
 DE_results <- read_tsv(DE_file, col_types = cols(
@@ -174,9 +180,6 @@ write_rds(gi, paste0(outPrefix, "gi_GR_pred.rds"))
 #-------------------------------------------------------------------------------
 # get genes
 #-------------------------------------------------------------------------------
-require(EnsDb.Hsapiens.v75)
-require(networkBMA)
-
 edb <- EnsDb.Hsapiens.v75
 ## Change the seqlevels style form Ensembl (default) to UCSC:
 seqlevelsStyle(edb) <- "UCSC"
@@ -214,26 +217,16 @@ tssGR <- resize(genesGR, width = 1, fix = "start")
 
 subGI <- gi[!is.na(gi$predBinary_GR_foldChange) & gi$predBinary_GR_foldChange]
 
-peaksGR <- peaks[[1]]
-
 #' Add colum to GenomicRanges object indicating any overlap
 #' 
-add_overalp <- function(tssGR, peakGR, colname = "peak_ovwerlap", ...){
+add_overalp <- function(tssGR, peaksGR, colname = "peak_ovwerlap", ...){
   
   mcols(tssGR)[, colname] <- GenomicRanges::countOverlaps(
     tssGR, peaksGR, ignore.strand = FALSE, ...) > 0
   return(tssGR)
 
-  }
+}
 
-tssGR <- add_overalp(tssGR, peaksGR, colname = "peak_ovwerlap", maxgap = 0)
-
-linksDF <- chromloop::linkRegions(tssGR, peaksGR, subGI) %>% 
-  as_tibble()
-
-tssGR$peak_loop <- seq(length(tssGR)) %in% linksDF$gr1
-
-tab <- table(tssGR$DE, tssGR$peak_loop)
 
 #' Get contingency table as tidy tibble from.
 #'
@@ -260,29 +253,35 @@ getContab <- function(true, pred){
   )
 }
 
+# helper function
+isLinked <- function(linkDF, len){1:len %in% linkDF$gr1}
+
 
 regPredDF <- tibble(
   maxgap = c(0, 10^3, 10^4, 10^5, 10^6),
-) %>% 
-  mutate(
-    gr = map(maxgap, ~ add_overalp(tssGR, peakGR, colname = paste0("peak_ovlerap_", .), maxgap = .)),
-    peak_ovlerap = map2(gr, maxgap, ~ mcols(.x)[, paste0("peak_ovlerap_", .y)]),
-    linkDF = map(maxgap, ~ chromloop::linkRegions(tssGR, peaksGR, subGI, maxgap = .)),
-    peak_loop = map(linkDF, ~ seq(length(tssGR)) %in% .$gr1),
   ) %>% 
-  select(maxgap, peak_ovlerap, peak_loop) %>% 
-  gather(key = method, value = pred, peak_ovlerap, peak_loop)
+  mutate(
+    gr = map(maxgap, ~ add_overalp(tssGR, peaksGR, colname = paste0("peak_ovlerap_", .), maxgap = .)),
+    ovlerap = map2(gr, maxgap, ~ mcols(.x)[, paste0("peak_ovlerap_", .y)]),
+    loop_maxgap = map(map(maxgap, ~ chromloop::linkRegions(tssGR, peaksGR, subGI, maxgap = .)), isLinked, length(tssGR)),
+    loop_inner_maxgap = map(map(maxgap, ~ chromloop::linkRegions(tssGR, peaksGR, subGI, inner_maxgap = .)), isLinked, length(tssGR)),
+    loop_inner_outer = map(map(maxgap, ~ chromloop::linkRegions(tssGR, peaksGR, subGI, inner_maxgap = ., outer_maxgap = .)), isLinked, length(tssGR)),
+    loop_inLoop = map(map(maxgap, ~ chromloop::linkRegionsInLoops(tssGR, peaksGR, subGI, maxgap = .)), isLinked, length(tssGR)),
+  ) %>% 
+  dplyr::select(maxgap, ovlerap, starts_with("loop_")) %>% 
+  gather(key = method, value = pred, ovlerap, starts_with("loop_"))
 
+write_rds(regPredDF, paste0(outPrefix, ".regPredDF.rds"))
+
+# calculate contingency tables and performance evaluations
 regGeneDF <- regPredDF %>% 
   mutate(
     contab = map(pred, ~ getContab(tssGR$DE == "DE", .x)),
     bc = map(contab, networkBMA::scores)
   ) %>% 
   unnest(bc) %>% 
-  select(-pred, -contab)
+  dplyr::select(-pred, -contab)
   
-
-
 write_tsv(regGeneDF, paste0(outPrefix, ".regGeneDF.tsv"))
 
 #-------------------------------------------------------------------------------
@@ -290,21 +289,43 @@ write_tsv(regGeneDF, paste0(outPrefix, ".regGeneDF.tsv"))
 #-------------------------------------------------------------------------------
 
 tidyRegDF <- regGeneDF %>% 
-  select(-expected, -`O/E`) %>% 
+  dplyr::select(-expected, -`O/E`) %>% 
   mutate(maxgap = as.factor(maxgap)) %>% 
   gather(key = "metric", value = "performance", TPR:ACC) %>% 
   filter(metric %in% c("F1", "precision", "recall", "sensitivity", "specificity", "MCC"))
 
-p <- ggplot(tidyRegDF, aes(x = method, y = performance, fill = maxgap, label = round(performance, 2))) +
+p <- ggplot(tidyRegDF, aes(x = maxgap, y = performance, fill = method, label = round(performance, 2))) +
   geom_bar(stat = "identity", position = "dodge") +
-  geom_text(hjust = "inward", position = position_dodge(.9), angle = 90) +
-  facet_wrap(~ metric) + 
+  geom_text(hjust = -.1, position = position_dodge(.9), angle = 90) +
+  facet_wrap(~ metric) + #, scales = "free_y"
+  ylim(0, 1.2) +
   theme_bw() +
   theme(
     text = element_text(size=10), 
     legend.position = "bottom",
-    axis.text.x = element_text(angle = 60, hjust = 1)) 
+    axis.text.x = element_text(angle = 60, hjust = 1)) +
+  scale_fill_manual(values = brewer.pal(5, "Set1"))
+p
 
-ggsave(paste0(outPrefix, ".regGeneDF.binary_classification.barplot.pdf"), w = 6, h = 6)
+ggsave(paste0(outPrefix, ".regGeneDF.binary_classification.barplot.pdf"), w = 12, h = 6)
+
+#-------------------------------------------------------------------------------
+# compare predicted regulated genes to log10(p-value) of expression
+#-------------------------------------------------------------------------------
+DEvsRegDF <- regPredDF %>% 
+  mutate(padj = list(genesGR$padj)) %>% 
+  unnest(pred, padj)
+
+p <- ggplot(DEvsRegDF, aes(x = pred, y = padj, color = method)) +
+  geom_boxplot() +
+  facet_wrap(~ maxgap) +
+  theme_bw() +
+  theme(
+    text = element_text(size=10), 
+    legend.position = "bottom",
+    axis.text.x = element_text(angle = 60, hjust = 1)) +
+  scale_color_manual(values = brewer.pal(5, "Set1"))
+
+ggsave(paste0(outPrefix, ".DEvsRegDF.padj_vs_binding.boxplot.pdf"), w = 6, h = 6)
 
 
