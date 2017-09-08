318c274ec6c0327c31f8d9274b4386cb5531b6f6
ibn-salem <jonas.ibn-salem@gmx.de>
2017-09-08 10:53:55 +0200

    add plot of ChIP signal at selected anchors and correlation

--- R/loop_features_EDA.R
+++ R/loop_features_EDA.R
@@ -8,6 +8,7 @@ require(tidyverse)    # for tidy data
 require(stringr)      # for string functions
 require(RColorBrewer)   # for nice colors
 require(feather)      # for efficient storing of data.frames
+require(InteractionSet)
 
 
 # 0) Set parameter --------------------------------------------------------
@@ -45,6 +46,8 @@ SELECTED_TF <- c(
 COL_SELECTED_TF = brewer.pal(12, "Paired")
 COL_SELECTED_TF_1 = brewer.pal(12, "Paired")[c(1, 3, 5, 7, 9, 11)]
 COL_SELECTED_TF_2 = brewer.pal(12, "Paired")[c(2, 4, 6, 8, 10, 12)]
+
+COL_ANCHOR = brewer.pal(12, "Paired")[c(2,6)] #[c(2,1,5,6)]
 
 #-------------------------------------------------------------------------------
 # Parse and filter input ChiP-seq data  -----------------------------------
@@ -90,9 +93,123 @@ tidyDF <- df %>%
 # tidySeltDF <- tidyDF %>% 
 #   filter(name %in% SELECTED_TF)
 
+# load GInteractions object
+gi <- read_rds(paste0(outPrefix, ".gi.rds"))
+
 #===============================================================================
 # Plot ChIP-seq coverage at anchors
 #===============================================================================
+# get list of coverage for all anchors
+
+FACT = "ZNF143"
+N_EXAMPLE = 3
+covList <- mcols(regions(gi))[,paste0("cov_", FACT)]
+
+# get randomly three loop and three non-loping pairs
+bothSupport <- gi$Loop_Rao_GM12878  == "Loop" & gi$Loop_Tang2015_GM12878 == "Loop"
+bothNoSupport <- gi$Loop_Rao_GM12878  != "Loop" & gi$Loop_Tang2015_GM12878 != "Loop"
+
+rand_examples = c(
+  sample(which(bothSupport), N_EXAMPLE),
+  sample(which(bothNoSupport), N_EXAMPLE)
+)
+write_rds(rand_examples, paste0(outPrefix, ".rand_examples.rds"))
+
+# Loop examples : 357242
+# No Loop example : 214045
+
+cov_up <- covList[anchors(gi[rand_examples], type = "first", id = TRUE)]
+cov_down <- covList[anchors(gi[rand_examples], type = "second", id = TRUE)]
+
+covDF <- tibble(
+    interaction_id = rand_examples,
+    loop = rep(c("Loop", "No loop"), each = 3),
+    reg = c(1:N_EXAMPLE, 1:N_EXAMPLE),
+    cov_up = as.list(cov_up),
+    cov_down = as.list(cov_down),
+  ) %>% 
+  gather(key = "anchor", value = "cov", cov_up, cov_down) %>% 
+  mutate(anchor = str_replace(anchor, "cov_", "")) %>% 
+  unnest(cov) %>% 
+  mutate(pos = rep(1:WINDOW_SIZE, n()/WINDOW_SIZE) - WINDOW_SIZE/2)
+
+write_feather(covDF, paste0(outPrefix, ".6_random_pairs.feather"))
+
+#-------------------------------------------------------------------
+# plotting coverage 
+#-------------------------------------------------------------------
+p = ggplot(covDF, aes(x = pos, fill = anchor)) +
+  geom_ribbon(aes(ymin = 0, ymax = cov)) +
+  facet_grid(reg ~ loop, scales = "free_y") +
+  theme_bw() + 
+  theme(text = element_text(size = 20), 
+        axis.text.x = element_text(angle = 45, hjust = 1), 
+        strip.text.y = element_blank()) + 
+  labs(y = paste(FACT, "ChIP-seq"), x = "Positions around CTCF motif") +
+  scale_fill_manual(values = alpha(COL_ANCHOR, 0.5)) 
+# + 
+  # coord_cartesian(ylim = c(0,8*mean(factDF$signal)))
+# p
+ggsave(p, file = paste0(outPrefix, ".6_random_pairs.pdf"), w = 7, h = 7)
+
+#-------------------------------------------------------------------
+# plot only two pairs sites
+#-------------------------------------------------------------------
+p = ggplot(filter(covDF, reg == 1), aes(x = pos, fill = anchor)) +
+  geom_ribbon(aes(ymin = 0, ymax = cov)) +
+  facet_grid(reg ~ loop, scales = "free_y") +
+  theme_bw() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), strip.text.y = element_blank()) + 
+  labs(y = paste(FACT, "ChIP-seq"), x = "Positions around CTCF motif") +
+  scale_fill_manual(values = alpha(COL_ANCHOR, 0.5)) 
+ggsave(p, file = paste0(outPrefix, ".2_random_pairs.pdf"), w = 7, h = 3.5)
+
+
+
+#-------------------------------------------------------------------
+# factAncDF <- cast(sub3, TF+pos+reg+support~anchor, value="signal")
+factAncDF <- covDF %>% 
+  spread(key = anchor, value = cov)
+
+
+corDF <- factAncDF %>% 
+  group_by(reg, loop) %>% 
+  summarize(
+    R = cor(up, down, use = "complete"),
+    p_val = cor.test(up, down)$p.value
+  )
+
+p = ggplot(factAncDF, aes(x = up, y = down, color = pos)) +
+  geom_point() +
+  facet_grid(reg ~ loop, scales = "free") +
+  geom_text(aes(x = max(factAncDF$up, na.rm = TRUE), y = 1, 
+                label = paste0("R=", round(R, 2))),
+            data = corDF, color = "black",
+            vjust = "inward", hjust = "inward", size = 7.5) +
+  theme_bw() + theme(strip.text.y = element_blank(), text = element_text(size = 20)) + 
+  xlab("UP anchor coverage") + ylab("DOWN anchor coverage") + 
+  # scale_color_gradient2(low = "grey", mid = scales::muted("blue"), high = "gray")
+  # scale_color_gradientn(colors = RColorBrewer::brewer.pal(3, "BrBG"))
+  scale_color_gradientn(colors = colorspace::rainbow_hcl(20))
+
+ggsave(p, file = paste0(outPrefix, ".6_random_pairs.cor.pdf"), w = 7, h = 7)
+
+subAncDF <- filter(factAncDF, reg == 1)
+
+p = ggplot(filter(subAncDF, reg == 1), aes(x = up, y = down, color = pos)) +
+  geom_point() +
+  facet_grid(reg ~ loop, scales = "free") +
+  geom_text(aes(x = max(subAncDF$up, na.rm = TRUE), y = 1, 
+                label = paste0("R=", round(R, 2))),
+            data =filter(corDF, reg == 1), color = "black",
+            vjust = "inward", hjust = "inward", size = 7.5) +
+  theme_bw() + theme(strip.text.y = element_blank(), text = element_text(size = 20)) + 
+  xlab("UP anchor coverage") + ylab("DOWN anchor coverage") + 
+  # scale_color_gradient2(low = "grey", mid = scales::muted("blue"), high = "gray")
+  # scale_color_gradientn(colors = RColorBrewer::brewer.pal(3, "BrBG"))
+  scale_color_gradientn(colors = colorspace::rainbow_hcl(20))
+
+ggsave(p, file = paste0(outPrefix, ".2_random_pairs.cor.pdf"), w = 7, h = 3.5)
 
 
 #===============================================================================
