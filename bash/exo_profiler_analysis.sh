e764500cccdbec3ea476529f3526f362d9800691
ibn-salem <jonas.ibn-salem@gmx.de>
2017-12-22 17:26:14 +0100

    update exo profiler analysis to +- 100 bp profile

--- bash/exo_profiler_analysis.sh
+++ bash/exo_profiler_analysis.sh
@@ -12,23 +12,25 @@ EXOPROFILER_DIR="../ExoProfiler"
 
 mkdir -p results/exo_profiler
 
-SAMPLE="SRR2312570"
+# SAMPLE="SRR2312570"
 for SAMPLE in "SRR2312570" "SRR2312571" ; do
   
   BAM=data/Tang2015/${SAMPLE}.flexcat_matched_barcode.fastq.bowtie2.hg19_filtered.bam
-  
+
   # sort and index bam file
   $SAMTOOLS sort $BAM > $BAM.sorted.bam
   $SAMTOOLS index $BAM.sorted.bam
-  
+
   python ${EXOPROFILER_DIR}/python/5primeCounter.py \
     --input_sites $LOOP_BED \
+    --size 100 \
     --input_format bed \
     --bam_file $BAM.sorted.bam  \
     --output_prefix results/exo_profiler/${SAMPLE}_loop_ExoProfile
 
   python ${EXOPROFILER_DIR}/python/5primeCounter.py \
     --input_sites $NON_LOOP_BED \
+    --size 100 \
     --input_format bed \
     --bam_file $BAM.sorted.bam  \
     --output_prefix results/exo_profiler/${SAMPLE}_non-loop_ExoProfile
